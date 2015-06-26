28f553bbc0b612b3f789a9f6629818c9efd6b219
Travis Briggs <audiodude@gmail.com>
2015-06-26 19:13:18 +0000

    Add caching.

--- scrape.py
+++ scrape.py
@@ -1,5 +1,13 @@
-from lxml import html, etree
+import os
+
+from lxml import html
+import pylibmc
 import requests
+
+try:
+  mc = pylibmc.Client([os.environ['MEMCACHIER_SERVERS']], binary=True)
+except KeyError:
+  mc = None
 
 def get_courts(root, marker_name):
   marker_anchors = root.xpath('//a[@name="%s"]' % marker_name)
@@ -14,12 +22,18 @@ def get_courts(root, marker_name):
         court = sibling.text
 
 def get_all_courts():
+  all_courts = None
+  if mc:
+    all_courts = mc.get('all_courts')
+  if all_courts:
+    return all_courts
+
   COURT_MARKERS = ('APCTS', 'DCCTS', 'BKCTS')
 
   resp = requests.get('https://www.pacer.gov/psco/cgi-bin/links.pl')
   root = html.fromstring(resp.text)
 
-  courts = []
+  all_courts = []
   for marker in COURT_MARKERS:
     for info in get_courts(root, marker):
       info_resp = requests.get(info['info_link'])
@@ -28,8 +42,11 @@ def get_all_courts():
         '//td[contains(text(), "Software Version")]')[0]
       software_version = software_td.getnext().text
       info['software_version'] = software_version
-      courts.append(info)
-  return courts
+      all_courts.append(info)
+
+  if mc:
+    mc.set('all_courts', all_courts, 43200)  # Cache for 12 hours
+  return all_courts
 
 if __name__ == '__main__':
   import json
