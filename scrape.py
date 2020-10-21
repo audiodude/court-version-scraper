0e42b1524aaf001dc9460b4a477d030663d20176
Travis Briggs <audiodude@gmail.com>
2020-10-20 21:18:51 -0700

    Rewrite scraper for new page

--- scrape.py
+++ scrape.py
@@ -4,6 +4,10 @@ from lxml import html
 import pylibmc
 import requests
 
+HEADERS = {
+    'User-Agent': 'court-version-scraper (2.0) <audiodude@gmail.com>'
+}
+
 mc = None
 try:
   servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
@@ -15,24 +19,6 @@ try:
 except:
   mc = None
 
-def get_courts(root, marker_name):
-  marker_anchors = root.xpath('//a[@name="%s"]' % marker_name)
-  for marker in marker_anchors:
-    court = None
-    court_link = None
-    for sibling in marker.itersiblings():
-      if (court and sibling.tag == 'a' and
-          'courtinfo' in sibling.get('href')):
-        yield {
-          'court': court,
-          'court_link': court_link,
-          'info_link': sibling.get('href')
-        }
-        court = None
-      elif not court and sibling.tag == 'a':
-        court = sibling.text
-        court_link = sibling.get('href')
-
 def get_all_courts(force=False):
   all_courts = None
   if mc:
@@ -40,32 +26,66 @@ def get_all_courts(force=False):
   if all_courts and not force:
     return all_courts
 
+  name_to_marker = {
+    'Appeals': ('U.S. Courts of Appeals', 'APCTS'),
+    'District': ('U.S. District Courts', 'DCCTS'),
+    'Bankruptcy': ('U.S. Bankruptcy Courts', 'BKCTS'),
+  }
   COURT_MARKERS = (('U.S. Courts of Appeals', 'APCTS'),
                    ('U.S. District Courts', 'DCCTS'),
                    ('U.S. Bankruptcy Courts', 'BKCTS'))
 
-  resp = requests.get('https://www.pacer.gov/psco/cgi-bin/links.pl')
+  resp = requests.get('https://pacer.uscourts.gov/file-case/court-cmecf-lookup',
+                      headers=HEADERS)
   root = html.fromstring(resp.text)
 
   all_courts = {}
 
-  for name, marker in COURT_MARKERS:
+  for tag, (name, marker) in name_to_marker.items():
     all_courts[name] = {'id': marker, 'courts': []}
-    for info in get_courts(root, marker):
-      info_resp = requests.get(info['info_link'])
-      info_root = html.fromstring(info_resp.text)
-      
-      software_td = info_root.xpath(
-        '//td[contains(text(), "Software Version")]')[0]
-      software_version = software_td.getnext().text
-      info['software_version'] = software_version
-      
-      go_live_td = info_root.xpath(
-        '//td[contains(text(), "ECF Go Live Date")]')[0]
-      software_go_live = go_live_td.getnext().text
-      info['software_go_live'] = software_go_live
-      
-      all_courts[name]['courts'].append(info)
+
+  for row in root.iterfind('.//tr'):
+    court_type = None
+    info = {}
+    for col in row.iterfind('./td'):
+      klass = col.get('class')
+      if 'court-name' in klass:
+        anchor = col.find('./a')
+        info['court'] = anchor.text.strip()
+        info['info_link'] = 'https://pacer.uscourts.gov%s' % anchor.get('href')
+      elif 'court-type' in klass:
+        court_type = col.text.strip()
+    
+    name, marker = name_to_marker.get(court_type, (None, None))
+    if marker is None:
+      continue
+
+    info_resp = requests.get(info['info_link'], headers=HEADERS)
+    info_root = html.fromstring(info_resp.text)
+
+    software_td = info_root.xpath(
+      '//td[contains(text(), "Software Version")]')
+    if software_td:
+      software_td = software_td[0]
+      info['software_version'] = software_td.getnext().text
+    else:
+      info['software_version'] = None
+
+    go_live_td = info_root.xpath(
+      '//td[contains(text(), "ECF Go Live Date")]')
+    if go_live_td:
+      go_live_td = go_live_td[0]
+      info['software_go_live'] = go_live_td.getnext().text
+    else:
+      info['software_go_live'] = None
+
+    court_header_div = info_root.xpath(
+      '//i[contains(@class, "fa-globe-americas")]')
+    if court_header_div:
+      court_header_div = court_header_div[0].getparent().getnext()
+      info['court_link'] = court_header_div.find('.//a').get('href')
+
+    all_courts[name]['courts'].append(info)
       
   if mc:
     mc.set('all_courts', all_courts)
